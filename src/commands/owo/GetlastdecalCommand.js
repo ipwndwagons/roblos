97c87bba92853f6e564609c30acc2204ba70a16c
ipwndwagons <104120562+ipwndwagons@users.noreply.github.com>
2022-05-23 13:57:58 +1200

    updated or somthing
    now has axios for a future command

---
+++ src/commands/owo/GetlastdecalCommand.js
@@ -0,0 +1,11 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+
+module.exports = class GetlastdecalCommand extends BaseCommand {
+  constructor() {
+    super('getlastdecal', 'owo', []);
+  }
+
+  run(client, message, args) {
+    message.channel.send('getlastdecal command works');
+  }
+}
--- startbot.cmd
+++ startbot.cmd
@@ -1,5 +1,6 @@
 winget install -e --id OpenJS.NodeJS
 cmd /c npm i discord.js 
 cmd /c npm i noblox.js
+cmd /c npm i axios
 npm run dev
 pause
