f9be71aff7b8cbaae68626d9062ff1331cdb2f1d
ipwndwagons <104120562+ipwndwagons@users.noreply.github.com>
2022-05-25 17:34:15 +1200

    added getlastdecal
    thank you stizzy cat for telling me how to use http requests

--- src/commands/owo/GetlastdecalCommand.js
+++ src/commands/owo/GetlastdecalCommand.js
@@ -1,11 +1,30 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const fs = require("fs")
+const axios = require("axios")
+const noblox = require('noblox.js')
+let cookie = fs.readFileSync('cookies.txt', 'utf8'); //gets cookie from the txt file
 
 module.exports = class GetlastdecalCommand extends BaseCommand {
   constructor() {
     super('getlastdecal', 'owo', []);
   }
 
-  run(client, message, args) {
-    message.channel.send(`still working on this command`);
+  async run(client, message, args) {
+    //thanks to stizzy cat and someone else for the help
+    console.log("yes")
+    const currentUser = await noblox.setCookie(cookie)
+    .catch(function(error) { //
+        message.channel.send(`somthing fucked up :sob:`)
+        console.log(error)});
+          await new Promise(r => setTimeout(r, 2000));
+      axios.get(`https://www.roblox.com/users/inventory/list-json?assetTypeId=13&cursor=&itemsPerPage=10&pageNumber=1&userId=${currentUser.UserID}`)
+      .then(async res => {
+          var data = res.data
+              var assetideeee = JSON.stringify(data.Data.Items[0].Item.AssetId);
+      
+              console.log(`${message.author} uploaded ${assetideeee}`) //temp log of assets uploaded (clears when bot is restarted)
+      message.channel.send(`https://www.roblox.com/library/${assetideeee}`
+      )}
+      )} 
+
   }
-}
